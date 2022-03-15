ccbcf7944ff5b79bd61c1e7eb6a0ecb40cc1b041
ancaicedo <77050076+ancaicedo@users.noreply.github.com>
2022-03-15 02:11:07 -0600

    staged some changes again...

---
+++ JFreeChart_Lab4/src/org/jfree/data/HashTest.java
@@ -0,0 +1,21 @@
+package org.jfree.data;
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class HashTest {
+    private Range testRange;
+    
+    @Before
+	public void setUp() throws Exception {
+        testRange = new Range(-10, 10);
+    }
+
+    @Test
+    
+
+    @After
+    public void tearDown() throws Exception {}
+}
